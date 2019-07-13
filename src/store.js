cbf8e913aa083f7fa94a98ec0b64ac1a76577ff7
Rabia-Jamil <rabiajamil251@gmail.com>
2019-07-13 11:45:58 +0500

    Add files via upload

---
+++ src/store.js
@@ -0,0 +1,42 @@
+export const muscles = [
+    'shoulders', 'chest', 'arms', 'back', 'legs'
+]
+
+export const exercises = [
+    {
+        id: 'overhead-press',
+        title: 'Overhead Press',
+        description: 'Delts exercise...',
+        muscles: 'shoulders'
+    },
+    {
+        id: 'dips',
+        title: 'Dips',
+        description: 'Tricops exercise',
+        muscles: 'arms'
+    },
+    {
+        id: 'barbell-curls',
+        title: 'Barbell-curls',
+        description: 'Chest exercise...',
+        muscles: 'chest'
+    },
+    {
+        id: 'pull-ups',
+        title: 'Pull ups',
+        description: 'Back and bicops exercise...',
+        muscles: 'back'
+    },
+    {
+        id: 'deadlifts',
+        title: 'Deadlifts',
+        description: 'Back and leg exercise...',
+        muscles: 'back'
+    },
+    {
+        id: 'squats',
+        title: 'Squate',
+        description: 'Legs exercise...',
+        muscles: 'legs'
+    }
+]
